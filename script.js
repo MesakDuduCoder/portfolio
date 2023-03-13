c721e094dbe9456d0f1e4e2afdde99920ed19566
MesakDuduCoder <duduaoa99@gmail.com>
2023-03-13 23:32:40 +0530

    Fix styling

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ let btn2 = document.querySelector("#close-menu");
 function display() {
  document.getElementById("header-top").style.filter = "blur(8px)";
  document.getElementById("menu").style.visibility = "visible";
+ document.getElementById("menu-items").style.visibility = "visible";
 }
 
 function closeDisplay() {
  document.getElementById("header-top").style.filter = "blur(0)";
  document.getElementById("menu").style.visibility = "hidden";
+ document.getElementById("menu-items").style.visibility = "hidden";
 }
 
 btn.addEventListener("click", display);
